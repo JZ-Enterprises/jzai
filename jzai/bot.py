9ec88c6e01a5eda1b1b38405dc4b802a78b2dd0a
Zack Allen <157924514+JZ-Enterprises-Owner@users.noreply.github.com>
2024-06-27 12:34:29 +1000

    Update bot.py

--- jzai/bot.py
+++ jzai/bot.py
@@ -119,7 +119,7 @@ def typewriter(txt):
         sleep(0.001)
 
 def check_for_updates():
-    current_version = "59.84.01"
+    current_version = "59.84.2"
     version_parts = list(map(int, current_version.split(".")))
 
     def version_to_str(parts):
